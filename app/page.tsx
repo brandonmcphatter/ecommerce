bf8f57d3a0fd75cd52fb85a7f9379a5e5f99b9a5
brandon <brandonmcphatter1@gmail.com>
2025-01-05 21:53:47 -0500

    added ClearanceBanner.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
 import TrendingSection from "@/components/TrendingSection/TrendingSection";
+import ClearanceBanner from "@/components/generalUse/ClearanceBanner";
 
 export default function Home() {
   return (
     <div>
       <main>
         <TrendingSection />
+          <ClearanceBanner />
       </main>
     </div>
   );
 }
-
-
-
-
