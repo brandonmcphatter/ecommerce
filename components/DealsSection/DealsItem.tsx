00f36c4eccd65e634554ecd5ed44240efd85c5dc
brandon <brandonmcphatter1@gmail.com>
2025-01-06 20:04:15 -0500

    items/cards for DealsSection.tsx on home page

---
+++ components/DealsSection/DealsItem.tsx
@@ -0,0 +1,10 @@
+export default function DealsItem({title}: {title: string}) {
+    return (
+        <div>
+            <div className={'dealsCard border-2 w-fit'}>
+
+            </div>
+            <h1 className={'text-center'}>{title}</h1>
+        </div>
+    )
+}
