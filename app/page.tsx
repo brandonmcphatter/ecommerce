311435b11d8332cd0eafe64da5b446d8e0d09596
brandon <brandonmcphatter1@gmail.com>
2025-01-06 20:05:15 -0500

    added dealsSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import TrendingSection from "@/components/TrendingSection/TrendingSection";
 import ClearanceBanner from "@/components/generalUse/ClearanceBanner";
+import DealsSection from "@/components/DealsSection/DealsSection";
 
 export default function Home() {
-  return (
-    <div>
-      <main>
-        <TrendingSection />
-          <ClearanceBanner />
-      </main>
-    </div>
-  );
+    return (
+        <div>
+            <main>
+                <TrendingSection/>
+                <ClearanceBanner/>
+                <DealsSection/>
+            </main>
+        </div>
+    );
 }
