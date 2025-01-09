fcfc909af97c709c83eb84e2dbc720833264a4bf
brandon <brandonmcphatter1@gmail.com>
2025-01-08 23:35:10 -0500

    section for home page

---
+++ components/ShopSaveSection/ShopSaveSection.tsx
@@ -0,0 +1,24 @@
+import ShopSaveCard from "@/components/ShopSaveSection/ShopSaveCard";
+
+export default function ShopSaveSection() {
+    const shopSave = {
+        description: "Link Your Prime: 2x VIP points and special deals.",
+        action: "Link Your Prime",
+        icon: "🔗"
+    }
+    return (
+        <section className={''}>
+            <div>
+                <h2>Ways to Shop & Save!</h2>
+            </div>
+
+            <div className={'flex flex-col md:flex-row gap-4'}>
+                <ShopSaveCard description={shopSave.description} action={shopSave.action} icon={shopSave.icon}/>
+                <ShopSaveCard description={shopSave.description} action={shopSave.action} icon={shopSave.icon}/>
+                <ShopSaveCard description={shopSave.description} action={shopSave.action} icon={shopSave.icon}/>
+            </div>
+
+        </section>
+    )
+}
+
