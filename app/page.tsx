eab1933dbb4961907c131fa497fc91415d9e2712
brandon <brandonmcphatter1@gmail.com>
2025-01-08 23:34:43 -0500

    added ShopSaveSection.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import TrendingSection from "@/components/TrendingSection/TrendingSection";
 import ClearanceBanner from "@/components/generalUse/ClearanceBanner";
 import DealsSection from "@/components/DealsSection/DealsSection";
 import NewArrivalsSection from "@/components/NewArrivalsSection/NewArrivalsSection";
+import ShopSaveSection from "@/components/ShopSaveSection/ShopSaveSection";
 
 export default function Home() {
     return (
@@ -11,6 +12,7 @@ export default function Home() {
                 <ClearanceBanner/>
                 <DealsSection/>
                 <NewArrivalsSection />
+                <ShopSaveSection />
             </main>
         </div>
     );
