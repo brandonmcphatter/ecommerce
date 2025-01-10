023d3c031f7d9b41a6103e2922245868d5530d21
brandon <brandonmcphatter1@gmail.com>
2025-01-09 23:58:04 -0500

    added JoinEmail section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import ClearanceBanner from "@/components/generalUse/ClearanceBanner";
 import DealsSection from "@/components/DealsSection/DealsSection";
 import NewArrivalsSection from "@/components/NewArrivalsSection/NewArrivalsSection";
 import ShopSaveSection from "@/components/ShopSaveSection/ShopSaveSection";
+import JoinEmail from "@/components/generalUse/JoinEmail";
 
 export default function Home() {
     return (
@@ -13,6 +14,7 @@ export default function Home() {
                 <DealsSection/>
                 <NewArrivalsSection />
                 <ShopSaveSection />
+                <JoinEmail />
             </main>
         </div>
     );
