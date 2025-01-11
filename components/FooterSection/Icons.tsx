bfc12f2a41132eea9ea46f377f2a80e9773ea424
brandon <brandonmcphatter1@gmail.com>
2025-01-10 23:32:57 -0500

    icons for each footerItem on mobile view

---
+++ components/FooterSection/Icons.tsx
@@ -0,0 +1,21 @@
+function PlusIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+            <path fillRule="evenodd"
+                  d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z"
+                  clipRule="evenodd"/>
+        </svg>
+
+    )
+}
+
+function MinusIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+            <path fillRule="evenodd" d="M4.25 12a.75.75 0 0 1 .75-.75h14a.75.75 0 0 1 0 1.5H5a.75.75 0 0 1-.75-.75Z"
+                  clipRule="evenodd"/>
+        </svg>
+    )
+}
+
+export {PlusIcon, MinusIcon};
