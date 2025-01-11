ada56ab5050c8c31f3e46c9dd244607a2324299c
brandon <brandonmcphatter1@gmail.com>
2025-01-11 17:24:42 -0500

    added image

--- components/DealsSection/DealsItem.tsx
+++ components/DealsSection/DealsItem.tsx
@@ -1,8 +1,10 @@
-export default function DealsItem({title}: {title: string}) {
+import Image from "next/image";
+
+export default function DealsItem({title, img}: {title: string, img: string}) {
     return (
         <div className={'flex flex-col gap-3 '}>
-            <div className={'dealsCard w-fit'}>
-
+            <div className={'dealsCard w-fit border-4 border-white hover:border-blue-300'}>
+                <Image className={'object-cover w-full h-full'} src={img} alt={'image'} width={400} height={300}/>
             </div>
             <h1 className={'text-center'}>{title}</h1>
         </div>
