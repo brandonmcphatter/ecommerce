031b3ecbb207afae295089847a20ddebace7601c
brandon <brandonmcphatter1@gmail.com>
2025-01-11 17:25:24 -0500

    adjusted spacing

--- components/generalUse/JoinEmail.tsx
+++ components/generalUse/JoinEmail.tsx
@@ -1,6 +1,6 @@
 export default function JoinEmail() {
     return (
-        <section className={'py-3 px-2 bg-blue-300 flex flex-col gap-4'}>
+        <section className={'py-3 bg-blue-300 flex flex-col gap-4 px-6 mb-8'}>
             <div className={'text-2xl font-bold'}>Join Our List for 10% Off</div>
             <p>Sign up for CoolKix emails - new subscribers get 10% off a future order! Plus, get early access to
                 sales, coupons, and more.</p>
