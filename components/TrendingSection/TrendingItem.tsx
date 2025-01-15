b649de0e64433a2e67a19628b209bf049e1e4cd1
brandon <brandonmcphatter1@gmail.com>
2025-01-14 19:54:16 -0500

    rounded border

--- components/TrendingSection/TrendingItem.tsx
+++ components/TrendingSection/TrendingItem.tsx
@@ -5,7 +5,7 @@ export default function TrendingItem({title, img}:{title: string, img: string}) 
     return (
         <Link href={'#'}>
             <div className={'flex flex-col justify-center items-center gap-1'}>
-                <div className={'trendingPhoto'}>
+                <div className={'trendingPhoto rounded'}>
                     <Image className={'object-cover w-full h-full'} src={img} alt={'image'} width={200} height={300}/>
                 </div>
                 <h3 className={'text-center underline underline-offset-4'}>{title}</h3>
