7ea14e43a960f3b059f1e77d36752881fe955573
brandon <brandonmcphatter1@gmail.com>
2025-01-14 19:54:59 -0500

    adding icons in progress

--- components/ShopSaveSection/ShopSaveCard.tsx
+++ components/ShopSaveSection/ShopSaveCard.tsx
@@ -1,12 +1,15 @@
-export default function ShopSaveCard({description, action, icon}: {description: string, action: string, icon: string}) {
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faAmazon} from "@fortawesome/free-brands-svg-icons";
+
+export default function ShopSaveCard({description, action, }: {description: string, action: string}) {
     return (
-        <div className={'bg-gray-400 rounded flex justify-between p-2'}>
+        <div className={'bg-gray-200 rounded flex justify-between p-2'}>
             <div className={'flex flex-col gap-3 w-3/4'}>
-                <h3 className={'font-bold text-2xl'}>{description}</h3>
+                <h3 className={'font-bold text-2xl md:text-sm'}>{description}</h3>
                 <p>{action}</p>
             </div>
             <div>
-                {icon}
+                <FontAwesomeIcon icon={faAmazon}/>
             </div>
         </div>
     )
