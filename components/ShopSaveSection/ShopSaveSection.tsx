b204b9f94523cb810de5d7871b28537509b1b474
brandon <brandonmcphatter1@gmail.com>
2025-01-14 19:54:48 -0500

    padded section and centered cards

--- components/ShopSaveSection/ShopSaveSection.tsx
+++ components/ShopSaveSection/ShopSaveSection.tsx
@@ -1,22 +1,24 @@
 import ShopSaveCard from "@/components/ShopSaveSection/ShopSaveCard";
 
+
 export default function ShopSaveSection() {
     const shopSave = {
         description: "Link Your Prime: 2x VIP points and special deals.",
         action: "Link Your Prime",
-        icon: "🔗"
+
     }
     return (
-        <section className={''}>
+        <section className={'px-6 grid gap-4'}>
             <div>
                 <h2 className={'font-bold text-xl'}>Ways to Shop & Save!</h2>
             </div>
 
-            <div className={'flex flex-col md:flex-row gap-4'}>
-                <ShopSaveCard description={shopSave.description} action={shopSave.action} icon={shopSave.icon}/>
-                <ShopSaveCard description={shopSave.description} action={shopSave.action} icon={shopSave.icon}/>
-                <ShopSaveCard description={shopSave.description} action={shopSave.action} icon={shopSave.icon}/>
+            <div className={'flex flex-col md:flex-row justify-center gap-4'}>
+                <ShopSaveCard description={shopSave.description} action={shopSave.action} />
+                <ShopSaveCard description={shopSave.description} action={shopSave.action} />
+                <ShopSaveCard description={shopSave.description} action={shopSave.action} />
             </div>
+
 
         </section>
     )
