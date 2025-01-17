be0719ebc16c9e493bc92e429258c8bc9f6c988d
brandon <brandonmcphatter1@gmail.com>
2025-01-16 22:36:35 -0500

    base layout for ShoppingBagScreen.tsx

--- components/generalUse/NavBar/ShoppingBagScreen.tsx
+++ components/generalUse/NavBar/ShoppingBagScreen.tsx
@@ -1,5 +1,12 @@
-export default function ShoppingBagScreen() {
+import {CloseIcon, ProfileIcon} from "@/components/generalUse/NavBar/Icons";
+
+export default function ShoppingBagScreen({toggleBag}: {toggleBag: () => void }) {
     return (
-        <div></div>
+        <div className={'absolute inset-x-0 top-0 h-svh'}>
+            <div className={'flex justify-between'}>
+                <ProfileIcon />
+                <span onClick={toggleBag}><CloseIcon /></span>
+            </div>
+        </div>
     )
 }
