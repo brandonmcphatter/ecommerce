1c1a7c638a8bb7a83367259a36ef7b152ced4f6c
brandon <brandonmcphatter1@gmail.com>
2025-01-16 22:36:06 -0500

    added ShoppingBagScreen.tsx to NavBar.tsx in progress

--- components/generalUse/NavBar/NavBar.tsx
+++ components/generalUse/NavBar/NavBar.tsx
@@ -2,27 +2,33 @@
 
 import {CloseIcon, MagnifyingGlass, MenuToggle, ProfileIcon, ShoppingBag} from "@/components/generalUse/NavBar/Icons";
 import {useState} from "react";
+import ShoppingBagScreen from "@/components/generalUse/NavBar/ShoppingBagScreen";
 
 export default function NavBar() {
     const [showSearch, setShowSearch] = useState(false);
+    const [showBag, setShowBag] = useState(false);
     const toggleSearch = () => setShowSearch(!showSearch);
+    const toggleBag = () => setShowBag(!showBag);
 
     return (
-        <nav className={'flex flex-col py-3 border-b px-6 mb-3 relative h-16'}>
-            {!showSearch && <div className={'flex justify-between items-center '}>
-                <div>
-                    <h1 className={'text-2xl font-bold'}>CoolKix</h1>
-                </div>
-                <div className={'flex gap-5'}>
-                    <span onClick={toggleSearch}><MagnifyingGlass/></span>
-                    <ShoppingBag/>
-                    <MenuToggle/>
-                    <span className={'hidden md:flex'}><ProfileIcon/></span>
-                </div>
-            </div>}
+        <>
+            <nav className={'flex flex-col py-3 border-b px-6 mb-3 relative h-16'}>
+                {!showSearch && <div className={'flex justify-between items-center '}>
+                    <div>
+                        <h1 className={'text-2xl font-bold'}>CoolKix</h1>
+                    </div>
+                    <div className={'flex gap-5'}>
+                        <span onClick={toggleSearch}><MagnifyingGlass/></span>
+                        <span onClick={toggleBag}><ShoppingBag/></span>
+                        <MenuToggle/>
+                        <span className={'hidden md:flex'}><ProfileIcon/></span>
+                    </div>
+                </div>}
 
+            </nav>
             {showSearch && <SearchBar toggleSearch={toggleSearch}/>}
-        </nav>
+            {showBag && <ShoppingBagScreen toggleBag={toggleBag}/>}
+        </>
 
     )
 }
