2067e72e16d9a5ffb72f65fc85638d4e25aa3a6a
brandon <brandonmcphatter1@gmail.com>
2025-01-22 22:27:15 -0500

    setting up layout for shoes page

--- app/shoes/page.tsx
+++ app/shoes/page.tsx
@@ -1,8 +1,8 @@
 export default function Shoes() {
     return (
-        <div className={'h-svh'}>
-            <div>SideBar</div>
-            <div>DisplayArea</div>
+        <div className={'h-svh flex'}>
+            <div className={'hidden md:flex md:w-1/4 lg:w-1/3 bg-blue-300'}>SideBar</div>
+            <div className={'w-full md:w-3/4 lg:w-2/3 bg-green-300'}>DisplayArea</div>
         </div>
     )
 }
