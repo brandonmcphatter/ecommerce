e84b66688a42fb2c7c16d03d68d18af74e4ebb66
brandon <brandonmcphatter1@gmail.com>
2025-02-03 23:39:58 -0500

    starting product page design

--- app/shoes/page.tsx
+++ app/shoes/page.tsx
@@ -1,7 +1,7 @@
 export default function Shoes() {
     return (
         <div className={'h-svh flex'}>
-            <div className={'hidden md:flex md:w-1/3 lg:w-1/4 bg-blue-300'}>SideBar</div>
+            <div className={'hidden md:flex md:w-1/3 lg:w-1/4 bg-blue-400'}>SideBar</div>
             <div className={'w-full md:w-2/3 lg:w-3/4 bg-green-300'}>DisplayArea</div>
         </div>
     )
