85088c92f0e2d9cf8880a84364c50a66064f4a64
brandon <brandonmcphatter1@gmail.com>
2025-02-17 23:44:10 -0500

    added links to trending items

--- components/TrendingSection/TrendingItem.tsx
+++ components/TrendingSection/TrendingItem.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function TrendingItem({title, img}:{title: string, img: string}) {
     return (
-        <Link href={'#'}>
+        <Link href={'/shoes'}>
             <div className={'flex flex-col justify-center items-center gap-1'}>
                 <div className={'trendingPhoto rounded'}>
                     <Image className={'object-cover w-full h-full'} src={img} alt={'image'} width={200} height={300}/>
