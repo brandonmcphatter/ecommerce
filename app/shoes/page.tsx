485588add551a39ccdc454c37bdecd6695529a05
brandon <brandonmcphatter1@gmail.com>
2025-03-12 22:12:52 -0400

    back on the horse

--- app/shoes/page.tsx
+++ app/shoes/page.tsx
@@ -1,8 +1,27 @@
 export default function Shoes() {
     return (
         <div className={'h-svh flex'}>
-            <div className={'hidden md:flex md:w-1/3 lg:w-1/4 bg-blue-400'}>SideBar</div>
+            <div className={'hidden md:flex md:w-1/3 lg:w-1/4 bg-blue-400'}>
+                <SideBar/>
+            </div>
             <div className={'w-full md:w-2/3 lg:w-3/4 bg-green-300'}>DisplayArea</div>
         </div>
+    )
+}
+
+function SideBar() {
+
+    return (
+        <ul className={'mx-auto'}>
+            <li className={'sideBarItem'}>Women&#39;s Shoe Size</li>
+            <li className={'sideBarItem'}>Men&#39;s Shoe Size</li>
+            <li className={'sideBarItem'}>Kids&#39; Show Size</li>
+            <li className={'sideBarItem'}>Brand</li>
+            <li className={'sideBarItem'}>Color</li>
+            <li className={'sideBarItem'}>Gender</li>
+            <li className={'sideBarItem'}>Product Type</li>
+            <li className={'sideBarItem'}>Sale</li>
+            <li className={'sideBarItem'}>Price</li>
+        </ul>
     )
 }
--- components/generalUse/NavBar/ShoppingBagScreen.tsx
+++ components/generalUse/NavBar/ShoppingBagScreen.tsx
@@ -1,12 +1,8 @@
-import {CloseIcon, ProfileIcon} from "@/components/generalUse/NavBar/Icons";
 
-export default function ShoppingBagScreen({toggleBag}: {toggleBag: () => void }) {
+export default function ShoppingBagScreen() {
     return (
-        <div className={'absolute inset-x-0 top-0 h-svh'}>
-            <div className={'flex justify-between'}>
-                <ProfileIcon />
-                <span onClick={toggleBag}><CloseIcon /></span>
-            </div>
+        <div className={'h-svh sideMenu'}>
+
         </div>
     )
 }
